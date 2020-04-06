8821247d65d64f2e35fb762ddd697ee00c861fa2
Celia Franceschi <franceschi.ce@gmail.com>
2020-04-06 15:00:40 +0200

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = 'https://the-good-wardrobe-v2.herokuapp.com'
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -39,7 +41,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
@@ -61,7 +63,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "the-good-wardrobe-v2_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "the-good-wardrobe_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
