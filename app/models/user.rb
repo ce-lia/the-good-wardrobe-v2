d5613f78bde4ee1c2d678cbdd22beca962ec7f22
Celia Franceschi <franceschi.ce@gmail.com>
2020-04-07 18:20:16 +0200

    need to add badges for average time

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,24 +8,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   def products_lifetime
-    discarded_statuses = ["sold", "recycled", "donated", "thrown away"]
-    discarded_clothes = []
     lifetime = 0
     average_lifetime = 0
       if products.count > 0
         products.each do |product|
-          if discarded_statuses.include?(product.status)
-            discarded_clothes << product
+          if product.discard_date != nil && product.purchase_date != nil
+            lifetime += (product.discard_date - product.purchase_date)
+          elsif product.discard_date == nil && product.purchase_date != nil
+            lifetime += (Date.today - product.purchase_date)
+          elsif product.discard_date != nil && product.purchase_date == nil
+            lifetime += (product.discard_date - product.created_at.to_date)
+          elsif product.discard_date == nil && product.purchase_date == nil
+            lifetime += (Date.today - product.created_at.to_date)
           end
         end
-        discarded_clothes.each do |discarded|
-          if discarded.purchase_date != nil
-            lifetime += (discarded.discard_date - discarded.purchase_date)
-          else
-            lifetime += (discarded.discard_date - discarded.created_at.to_date)
-          end
-        end
-        average_lifetime = ((lifetime / discarded_clothes.count) / 365 )* 12
+        average_lifetime = (lifetime / products.count.to_f) / 365
       end
     average_lifetime
   end
